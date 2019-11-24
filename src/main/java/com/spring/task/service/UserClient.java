cf722fbc92ea3c5754609b7c0ef9076f3cf6d5ab
amartiros <martirosyan.arthur@yahoo.com>
2019-11-24 18:43:31 +0100

    Created new endpoint for get user by id

---
+++ src/main/java/com/spring/task/service/UserClient.java
@@ -0,0 +1,52 @@
+package com.spring.task.service;
+
+import com.spring.task.model.Post;
+import com.spring.task.model.User;
+import com.spring.task.model.UserResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class UserClient {
+
+    private static final String GET_USER_URI = "/users/{userId}";
+    private static final String GET_POSTS_URI = "/posts";
+
+    private WebClient webClient;
+
+    public UserClient(@Value("${services.url}") final String url) {
+        this.webClient = WebClient.create(url);
+    }
+
+    public Flux<UserResponse> fetchUserAndPosts(final Long userId) {
+        log.info("Calling fetchUserAndPosts by userId:{}", userId);
+
+        final Mono<User> user = getUser(userId).subscribeOn(Schedulers.elastic());
+        final Mono<List<Post>> items = getPosts(userId).subscribeOn(Schedulers.elastic());
+
+        return Flux.zip(user, items, UserResponse::new);
+    }
+
+    private Mono<User> getUser(final Long userId) {
+        return webClient.get()
+                .uri(GET_USER_URI, userId)
+                .retrieve()
+                .bodyToMono(User.class);
+    }
+
+    private Mono<List<Post>> getPosts(final Long userId) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path(GET_POSTS_URI)
+                        .queryParam("userId", userId).build())
+                .retrieve()
+                .bodyToFlux(Post.class).collectList();
+    }
+}
